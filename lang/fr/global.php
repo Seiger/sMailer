c1a0191c7fd7338460143ede7824f8fb0e3ce38e
Serhii Korneliuk <seiger@meta.ua>
2026-05-06 17:47:49 +0300

    [ADD] French.

---
+++ lang/fr/global.php
@@ -0,0 +1,18 @@
+<?php return [
+    'configure' => 'Paramètres',
+    'configure_help' => 'Configurer les paramètres de base des e-mails.',
+    'configure_icon' => 'fa fa-cog',
+    'mailer' => 'Envois',
+    'mailer_help' => 'Envoi des mails aux abonnés du site.',
+    'mailer_icon' => 'fa fa-envelope',
+    'once' => 'Envoi unique',
+    'once_help' => 'Configurer les paramètres de base pour les envois uniques.',
+    'once_icon' => 'fa fa-satellite-dish',
+    'periodic' => 'Envois périodiques',
+    'periodic_help' => 'Configurer les paramètres de base pour les envois périodiques.',
+    'periodic_icon' => 'fa fa-mail-bulk',
+    'subscribers' => 'Abonnés',
+    'subscribers_help' => 'Gérer la newsletter pour les abonnés.',
+    'subscribers_icon' => 'svg',
+    'subscribers_svg_icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H322.8c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1H178.3zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"/></svg>',
+];
